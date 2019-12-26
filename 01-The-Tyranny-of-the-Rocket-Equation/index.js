d9c0a1b75e24e2192fc0273ce62792d5f50dc6ea
RafaelGSS <rafael.nunu@hotmail.com>
2019-12-26 11:05:18 -0300

    feat: solve day1

--- 01-The-Tyranny-of-the-Rocket-Equation/index.js
+++ 01-The-Tyranny-of-the-Rocket-Equation/index.js
@@ -1,3 +1,10 @@
-function main(input) {
-  return Math.floor(input / 3 - 2)
+const input = require('./input')
+
+function calcMass(input) {
+  return input
+    .map(mass => Math.floor(mass / 3) - 2)
+    .reduce((prev, curr) => prev + curr, 0)
 }
+
+const response = calcMass(input)
+console.log(`Response: ${response}`)

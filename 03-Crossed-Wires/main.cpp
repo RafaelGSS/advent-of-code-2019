e3debeda951df865e66e45702882c4cc49e1ccd7
RafaelGSS <rafael.nunu@hotmail.com>
2019-12-28 04:36:36 -0300

    update(day3): add slow solution

--- 03-Crossed-Wires/main.cpp
+++ 03-Crossed-Wires/main.cpp
@@ -3,6 +3,10 @@
 #include <fstream>
 #include <memory>
 #include <map>
+#include <list>
+#include <algorithm>
+#include <stdlib.h>
+
 
 std::vector<std::string> split(const std::string& str, const std::string& delim)
 {
@@ -25,67 +29,103 @@ std::vector<std::string> split(const std::string& str, const std::string& delim)
   return tokens;
 }
 
-typedef std::vector<std::vector<int>> Board;
+struct Point {
+  int m_x;
+  int m_y;
 
-std::vector<std::vector<std::string>> get_input_data() {
-  std::fstream file;
-  std::vector<std::vector<std::string>> fdata;
-  const std::string delim = ",";
+  Point() {}
+  Point(int x, int y): m_x(x), m_y(y) {}
+  Point(Point& p): m_x(p.m_x), m_y(p.m_y) {}
+  Point(Point* p): m_x(p->m_x), m_y(p->m_y) {}
+};
 
-  file.open("./input.txt");
-  if (file.is_open()) {
-    std::string data;
-    while(std::getline(file, data)) {
-      fdata.push_back(split(data, delim));
+typedef std::list<Point*> Board;
+
+class Day3 {
+  public:
+    Day3() {
+      this->nearest_intersection = -1;
+      this->current_point = new Point(0, 0);
     }
-  }
 
-  return fdata;
-}
+    std::vector<std::vector<std::string>> get_input_data() {
+      std::fstream file;
+      std::vector<std::vector<std::string>> fdata;
+      const std::string delim = ",";
 
-void walk_direction(Board& board, const int& walks, int xDir, int yDir) {
-  int x = xDir;
-  int y = yDir;
-  for (int i = 0; i < walks; i++) {
-    std::cout << "Trying [" << x << "," << y << "]" << std::endl;
-    /* board[x][y]++; */
-    x += xDir;
-    y += yDir;
-  }
-}
+      file.open("./input.txt");
+      if (file.is_open()) {
+        std::string data;
+        while(std::getline(file, data)) {
+          fdata.push_back(split(data, delim));
+        }
+      }
 
-void process_instructions(std::vector<std::string> instructions, Board& board) {
-  for (auto& ins : instructions) {
-    const char& dir = ins[0];
-    const int& walks = std::stoi(ins.substr(1, ins.length()));
-    switch (dir) {
-      case 'U':
-        walk_direction(board, walks, 1, 0);
-        break;
-      case 'L':
-        walk_direction(board, walks, -1, 0);
-        break;
-      case 'D':
-        walk_direction(board, walks, 0, -1);
-        break;
-      case 'R':
-        walk_direction(board, walks, 0, 1);
-        break;
-      default:
-        std::cout << "Invalid direction! Exiting..." << std::endl;
-        exit(1);
+      return fdata;
     }
-    std::cout << "Instruction is: " << dir <<  " - Walks: " << walks << std::endl;
-  }
-}
+
+    void walk_direction(const int& walks, int x_distance, int y_distance) {
+      for (int i = 0; i < walks; i++) {
+        this->current_point->m_x += x_distance;
+        this->current_point->m_y += y_distance;
+        // slow!
+        auto found = std::find_if(this->board.begin(), this->board.end(), [&](Point* p) {
+            return p->m_x == this->current_point->m_x && p->m_y == this->current_point->m_y;
+            });
+
+        if (found != this->board.end()) {
+          int manhattan_distance = abs(this->current_point->m_x) + abs(this->current_point->m_y);
+          if (manhattan_distance < this->nearest_intersection) {
+            this->nearest_intersection = manhattan_distance;
+          }
+        } else {
+          this->board.push_back(new Point(this->current_point));
+        }
+
+      }
+    }
+
+    void process_instructions(std::vector<std::string> instructions) {
+      for (auto& ins : instructions) {
+        const char& dir = ins[0];
+        const int& walks = std::stoi(ins.substr(1, ins.length()));
+        switch (dir) {
+          case 'U':
+            walk_direction(walks, 1, 0);
+            break;
+          case 'L':
+            walk_direction(walks, -1, 0);
+            break;
+          case 'D':
+            walk_direction(walks, 0, -1);
+            break;
+          case 'R':
+            walk_direction(walks, 0, 1);
+            break;
+          default:
+            std::cout << "Invalid direction! Exiting..." << std::endl;
+            exit(1);
+        }
+      }
+    }
+
+    int get_nearest_intersection() {
+      return this->nearest_intersection;
+    }
+  private:
+    Board board;
+    Point* current_point;
+    int nearest_intersection;
+};
+
 
 int main() {
-  const auto fdata = get_input_data();
-  Board board(100000);
+  Day3* day = new Day3();
+  const auto fdata = day->get_input_data();
 
   for (auto& instructions : fdata) {
-    process_instructions(instructions, board);
-    std::cout << "Done!" << std::endl;
+    day->process_instructions(instructions);
+    std::cout << "Done = " <<  day->get_nearest_intersection() << std::endl;
     exit(0);
   }
 

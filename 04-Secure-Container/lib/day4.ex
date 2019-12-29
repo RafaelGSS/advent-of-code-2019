bd57f804800a15e40861b316002b95211f13289e
RafaelGSS <rafael.nunu@hotmail.com>
2019-12-29 00:37:38 -0300

    feat(day4): done day4

--- 04-Secure-Container/lib/day4.ex
+++ 04-Secure-Container/lib/day4.ex
@@ -20,21 +20,37 @@ defmodule Day4 do
     digits === Enum.sort(digits)
   end
 
-  def assert_rules(num) do
+  defp has_proper_double?(num) do
+    num
+    |> Integer.digits
+    |> Enum.chunk_by(& &1)
+    |> Enum.any?(&(Enum.count(&1) === 2))
+  end
+
+  def first_part_rules(num) do
     has_double?(num) and never_decrease?(num)
   end
 
-  def process_range([from, to]) do
+  def second_part_rules(num) do
+    first_part_rules(num) and has_proper_double?(num)
+  end
+
+  def process_range([from, to], func) do
     for(x <- from..to, do: x)
-    |> Enum.filter(&assert_rules/1)
+    |> Enum.filter(fn n -> func.(n) end)
     |> Enum.count
   end
 
   def main() do
     res =
       get_input_data()
-      |> process_range
+      |> process_range(&first_part_rules/1)
 
-    IO.puts res
+    res2 =
+      get_input_data()
+      |> process_range(&second_part_rules/1)
+
+    IO.puts "Part one: #{res}"
+    IO.puts "Part two: #{res2}"
   end
 end

fae0f2a3c7f94320570a073a923907468476c5d9
RafaelGSS <rafael.nunu@hotmail.com>
2019-12-29 15:30:36 -0300

    feat(day1): add forget part_two

--- 01-The-Tyranny-of-the-Rocket-Equation/index.js
+++ 01-The-Tyranny-of-the-Rocket-Equation/index.js
@@ -1,15 +1,30 @@
 const input = require('./input')
 
-function calcMass(input) {
+const calcFuel = mass => Math.floor(mass / 3) - 2
+
+const calcFullFuel = (mass, sum) => {
+  const value = calcFuel(mass)
+
+  if (value <= 0) {
+    return sum
+  }
+  return calcFullFuel(value, sum + value)
+}
+
+function calcMass(input, calc = calcFuel) {
   return input
-    .map(mass => Math.floor(mass / 3) - 2)
+    .map(calc)
     .reduce((prev, curr) => prev + curr, 0)
 }
 
 function main() {
   const response = calcMass(input)
-  console.log(`Response: ${response}`)
+  console.log(`Part one: ${response}`)
+  const response2 = calcMass(input, n => calcFullFuel(n, 0))
+  console.log(`Part two: ${response2}`)
 }
+
+main()
 
 module.exports = {
   calcMass
